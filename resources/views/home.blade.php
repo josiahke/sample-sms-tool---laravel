0cb0c4b7d44db795160b1ebd25e3b75d4b436b2b
josiah <josina08@gmail.com>
2018-09-17 15:02:30 +0300

    mod add forms and validations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,13 +14,13 @@
 
                     <div class="form-group">
                         <label for="exampleInputEmail1">Phone number</label>
-                        <input type="tel" class="form-control" id="phone" placeholder="Enter phone number">
+                        <input type="tel" class="form-control" id="phone" maxlength="14" minlength="12" placeholder="Enter phone number">
                     </div>
 
                     <div class="form-group">
                         <label for="exampleInputEmail1">SMS (<span class="pull-right" id="countchars">0</span>) </label>
                         {{--<input type="tel" class="form-control" id="phone" placeholder="Enter phone number">--}}
-                        <textarea name="sms" class="form-control"> </textarea>
+                        <textarea name="sms" id="sms" class="form-control" maxlength="160" minlength="1" > </textarea>
                     </div>
 
                     {!! Form::close() !!}
@@ -54,4 +54,20 @@
 
     </div>
 </div>
+@endsection
+
+
+@section('js')
+    <script >
+        var textarea = document.querySelector("textarea");
+        textarea.addEventListener("input", function(){
+            var maxlength = this.getAttribute("maxlength");
+            var currentLength = this.value.length;
+            if( currentLength >= maxlength ){
+                document.getElementById('countchars').innerHTML("You have reached the maximum number of characters.");
+            }else{
+                document.getElementById('countchars').innerHTML(maxlength - currentLength + " chars left");
+            }
+        });
+    </script>
 @endsection
